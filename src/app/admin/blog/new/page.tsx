642a58d85c5c49d0ec1f38b7c1d29d2edfdebfbe
Aayush Mistri <aayushhmistri@gmail.com>
2026-07-02 21:59:13 +0530

    pixel art added

--- src/app/admin/blog/new/page.tsx
+++ src/app/admin/blog/new/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import styles from "./new-blog.module.css";
 
 export default function NewBlogPage() {
